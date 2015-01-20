94d1319d3e50d052f7f896216a22c72b8aa91900
Marisa <mbernar@us.ibm.com>
2015-01-20 13:50:30 -0500

    Update forms.py

--- slick/blueprints/vm/forms.py
+++ slick/blueprints/vm/forms.py
@@ -13,7 +13,7 @@ class CreateVMForm(Form):
     memory = SelectField('Memory (GB)', validators=[Required()])
     network = RadioField(choices=[('10', '10Mbps'), ('100', '100Mbps'),
                                   ('1000', '1000 Mbps')])
-    tags = StringField('Tags')
+    tag = StringField('Tags')
 
 
 class EditVMForm(Form):
